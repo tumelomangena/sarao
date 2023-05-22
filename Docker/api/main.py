96fb42fd0cf8c195c524cf569d46b18d490abd73
tumelo <tmangena@ska.ac.za>
2023-05-22 08:42:02 +0200

    modified main.py script and updated the docker compose file

--- Docker/api/main.py
+++ Docker/api/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
-import os
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import ElasticVectorSearch
 
-app = FastAPI()
+from config import openai_api_key
+
+embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+db = ElasticVectorSearch(
+    elasticsearch_url="http://localhost:9200",
+    index_name="elastic-index",
+    embedding=embedding,
+)
+qa = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(temperature=0),
+    chain_type="stuff",
+    retriever=db.as_retriever(),
+)
 
 @app.get("/books")
-async def api_data():
-    return os.system("open \"\" https://localhost:9200/books/_search")
-
+async def index():
+    return {"books": db}
 
 @app.get("/divide")
 async def divide(dividend: int, divisor: int):
