f35d9f7efb7c76e6c0bec3a51f9542517f86b2a6
tumelo <tmangena@ska.ac.za>
2024-02-02 14:36:16 +0200

    latest learnings

--- Docker/api/main.py
+++ Docker/api/main.py
@@ -1,34 +1,14 @@
 from fastapi import FastAPI
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import ElasticVectorSearch
+from elasticsearch import Elasticsearch
 
-from config import openai_api_key
+app = FastAPI()
 
-embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+es = Elasticsearch(
+    hosts=["https://localhost:9200"], 
+    basic_auth=['elastic', 'OnEiLeMangEna'], 
+    verify_certs=False)
 
-db = ElasticVectorSearch(
-    elasticsearch_url="http://localhost:9200",
-    index_name="elastic-index",
-    embedding=embedding,
-)
-qa = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(temperature=0),
-    chain_type="stuff",
-    retriever=db.as_retriever(),
-)
 
 @app.get("/books")
 async def index():
-    return {"books": db}
-
-@app.get("/divide")
-async def divide(dividend: int, divisor: int):
-    return {"quotient":dividend/divisor}
-
-
-@app.post("/divide")
-async def divide(dividend: int, divisor: int):
-    return {"quotient":dividend/divisor}
-
+    return {"books": es.search()}
