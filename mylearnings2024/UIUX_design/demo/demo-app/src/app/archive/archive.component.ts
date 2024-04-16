b064ccf1111caf2eef79b3d7647362ce7f07a9fe
tumelomangena <tmangena@ska.ac.za>
2024-04-16 11:52:22 +0200

    creating an angular app for demo

---
+++ mylearnings2024/UIUX_design/demo/demo-app/src/app/archive/archive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archive',
+  standalone: true,
+  imports: [],
+  templateUrl: './archive.component.html',
+  styleUrl: './archive.component.css'
+})
+export class ArchiveComponent {
+
+}
---
+++ mylearnings2024/UIUX_design/demo/demo-app/src/app/proposal/proposal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proposal',
+  standalone: true,
+  imports: [],
+  templateUrl: './proposal.component.html',
+  styleUrl: './proposal.component.css'
+})
+export class ProposalComponent {
+
+}
